c39feb81ee6ab9175a5aa785f5f53c2af77be1f1
Guilherme Teixeira <41587365+guiteixeira-io@users.noreply.github.com>
2025-02-12 22:49:47 -0300

    initial charge

---
+++ index.py
@@ -0,0 +1,35 @@
+pedra = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+papel = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+tesoura = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+opcoes = [pedra,papel,tesoura]
+print("Vamos jogar pedra, papel, tesoura?")
+print("Digite 0 para Pedra, 1 para Papel e 2 para Tesoura")
+escolha = input("Qual você escolhe?\n")
+opcao = opcoes[int(escolha)] 
+print(f"{opcao}")
+p2 = ramdom.
+print("Eu escolho:")
+
