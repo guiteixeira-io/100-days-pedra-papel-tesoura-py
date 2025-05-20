1d150b0d01dffc588e6003238a013e813607ab2b
Guilherme Teixeira <41587365+guiteixeira-io@users.noreply.github.com>
2025-05-20 20:54:38 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+import random
+
 pedra = '''
     _______
 ---'   ____)
@@ -24,12 +26,20 @@ tesoura = '''
       (____)
 ---.__(___)
 '''
-opcoes = [pedra,papel,tesoura]
+opcoes = [pedra, papel, tesoura]
 print("Vamos jogar pedra, papel, tesoura?")
 print("Digite 0 para Pedra, 1 para Papel e 2 para Tesoura")
-escolha = input("Qual você escolhe?\n")
-opcao = opcoes[int(escolha)] 
-print(f"{opcao}")
-p2 = ramdom.
-print("Eu escolho:")
+escolha_p1 = int(input("Qual você escolhe?\n"))
+print(opcoes[escolha_p1])
+print("Eu escolho:\n")
+escolha_p2 = random.randint(0, 2)
+print(opcoes[escolha_p2])
 
+if escolha_p1 == escolha_p2:
+    print("Empate!")
+elif (escolha_p1 == 0 and escolha_p2 == 2) or \
+     (escolha_p1 == 1 and escolha_p2 == 0) or \
+     (escolha_p1 == 2 and escolha_p2 == 1):
+    print("Você ganhou!")
+else:
+    print("Você perdeu!")
